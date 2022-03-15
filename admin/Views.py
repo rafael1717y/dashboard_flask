5eaa00d89b7d6ae46649c1e6e90c188025f7cc40
rafael1717y <rafael1717yy@gmail.com>
2022-03-15 13:02:44 -0300

    final

--- admin/Views.py
+++ admin/Views.py
@@ -3,72 +3,64 @@ from flask_admin import AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect
-from model import *
 
 from config import app_config, app_active
+from model import *
 config = app_config[app_active]
 
-
 class HomeView(AdminIndexView):
-    extra_css = ['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css']
+    extra_css = ['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css','/static/css/home.css']
     extra_js = ['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js', '/static/js/chart.js']
 
     @expose('/')
     def index(self):
-        states = State.query.all()  #objeto da model traz todo mundo
+        states = State.query.all()
         diseases = Disease.query.all()
         estadoSaude = DiseaseState.query.all()
 
         return self.render('home.html', states=states, diseases=diseases, estadoSaude=estadoSaude)
 
     def is_accessible(self):
-        return True
+        return current_user.is_authenticated
 
-    """
-    def inaccessible_callback(self): #pega o current_user(logado)
+    def inaccessible_callback(self, name, **kwargs):
         if current_user.is_authenticated:
-            return redirect('/admin')  # se logado redireciona para admin -- senao para login
+            return redirect('/admin')
         else:
             return redirect('/login')
-    """
 
+class UserView(ModelView):
+    column_exclude_list = ['password']
+    form_excluded_columns = ['last_update']
 
-class UserView(ModelView): #deve ser separada criptografia
-    column_exclude_list = ['password'] 
-    form_exclude_columns= ['last_update']
-    
     form_widget_args = {
         'password': {
-            'type': 'password',
+            'type': 'password'
         }
     }
 
-
-    def on_model_change(self, form, User, is_created):  #quando for feita uma mudança na model
-        if 'password' in form: # verifica se está enviando uma senha no form e usa set password que está na model.py como sha256
+    def on_model_change(self, form, User, is_created):
+        if 'password' in form:
             if form.password.data is not None:
                 User.set_password(form.password.data)
             else:
                 del form.password
 
     def is_accessible(self):
-        return True
+        return current_user.is_authenticated
 
-
-
-
-class GenericView(ModelView):
-    def is_accessible(self):
-        return True
-
-    """
     def inaccessible_callback(self,name,**kwargs):
         if current_user.is_authenticated:
             return redirect('/admin')
         else:
             return redirect('/login')
-    """
 
+class GenericView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
 
-
-
+    def inaccessible_callback(self,name,**kwargs):
+        if current_user.is_authenticated:
+            return redirect('/admin')
+        else:
+            return redirect('/login')
--- controllers.py
+++ controllers.py
@@ -1,5 +1,23 @@
 from model import *
+from sqlalchemy import or_
 
+def login(email, password):
+    try:
+        user = User.query.filter(or_(User.email==email, User.username==email)).first()
+        if user:
+            result = user.verify_password(password, user.password)
+            if result:
+                return user
+    except Exception as e:
+        print(e)
+
+    return None
+
+def getUserById(id):
+    try:
+        return User.query.filter_by(id=id).first()
+    except:
+        return None
 
 def reportByState(state, disease):
     patients = db.session.query(db.func.count(Patient.estadoSaude), Patient.last_state, Patient.state).group_by(Patient.last_state).group_by(Patient.state)
